8a70854d145bf3177c55849eb690f7aa08289f34
yseeuult <167465395+yseeuult@users.noreply.github.com>
2024-04-19 17:36:15 +0200

    Update Introduction.svelte

--- src/Introduction.svelte
+++ src/Introduction.svelte
@@ -1,6 +1,5 @@
 <p> <strong> Dans le cadre du Pacte vert, lUE veut promouvoir léconomie circulaire en produisant moins de déchets tout en recyclant mieux. Cette politique vise surtout les résidus demballage. </strong></p>
 <p>Une fois quun produit est consommé, il reste les contenants : bouteilles plastiques, cartons de céréales, boites à chaussures, aluminium dun plat. Un des buts du Pacte Vert, cest de réduire les emballages en les recyclant. 
-
 <h2> La position de la Belgique en Europe </h2>
 <p> Selon <a href="https://ec.europa.eu/eurostat/databrowser/view/cei_wm020/default/table?lang=fr"> Eurostat </a>, les Belges ont produits en moyenne 170,8 kilogrammes de déchets demballages en 2021 mais 80,4% de ces déchets étaient recyclés. En 2021, 64% des emballages étaient recyclés en Europe. La Belgique se hisse donc en  première place du podium en étant le pays qui recycle le plus les déchets emballés.</p> 
 <p> Concernant son <strong> objectif 2025 </strong>, lEurope est encore en dessous de son objectif den recycler au moins 65%. On remarque aussi une grande disparité entre certains pays européens. Si la Belgique montre lexemple avec un taux de recyclage de 80,4%, certains pays comme Malte affichent un taux de 38,4%. 
