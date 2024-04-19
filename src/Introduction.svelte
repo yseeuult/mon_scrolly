413086fbc047bfc1d27ce0a7c60475d9efd893cd
yseeuult <167465395+yseeuult@users.noreply.github.com>
2024-04-19 17:35:28 +0200

    Update Introduction.svelte

--- src/Introduction.svelte
+++ src/Introduction.svelte
@@ -1,10 +1,11 @@
-<p> <strong> Dans le cadre du Pacte vert, lUE veut promouvoir léconomie circulaire en produisant moins de déchets tout en recyclant mieux. Cette politique vise surtout les résidus demballage. </strong></p>
-<p>Une fois quun produit est consommé, il reste les contenants : bouteilles plastiques, cartons de céréales, boites à chaussures, aluminium dun plat. Un des buts du Pacte Vert, cest de réduire les emballages en les recyclant. 
+<p> <strong> Dans le cadre du Pacte vert, lUE veut promouvoir lÃ©conomie circulaire en produisant moins de dÃ©chets tout en recyclant mieux. Cette politique vise surtout les rÃ©sidus demballage. </strong></p>
+<p>Une fois quun produit est consommÃ©, il reste les contenants : bouteilles plastiques, cartons de cÃ©rÃ©ales, boites Ã  chaussures, aluminium dun plat. Un des buts du Pacte Vert, cest de rÃ©duire les emballages en les recyclant. 
+
 <h2> La position de la Belgique en Europe </h2>
-<p> Selon <a href="https://ec.europa.eu/eurostat/databrowser/view/cei_wm020/default/table?lang=fr"> Eurostat </a>, les Belges ont produits en moyenne 170,8 kilogrammes de déchets demballages en 2021 mais 80,4% de ces déchets étaient recyclés. En 2021, 64% des emballages étaient recyclés en Europe. La Belgique se hisse donc en  première place du podium en étant le pays qui recycle le plus les déchets emballés.</p> 
-<p> Concernant son <strong> objectif 2025 </strong>, lEurope est encore en dessous de son objectif den recycler au moins 65%. On remarque aussi une grande disparité entre certains pays européens. Si la Belgique montre lexemple avec un taux de recyclage de 80,4%, certains pays comme Malte affichent un taux de 38,4%. 
-Malte et Roumanie font partie des « mauvais élèves » (avec 38,4% et 39,9% en 2021 et 2020). Et derrière la Belgique, viennent les Pays-Bas (76,6%), le Luxembourg (73,7%), lItalie (72,9%) et les autres pays suivent.</p> 
-<h2> L'évolution du recyclage en Belgique </h2>
+<p> Selon <a href="https://ec.europa.eu/eurostat/databrowser/view/cei_wm020/default/table?lang=fr"> Eurostat </a>, les Belges ont produits en moyenne 170,8 kilogrammes de dÃ©chets demballages en 2021 mais 80,4% de ces dÃ©chets Ã©taient recyclÃ©s. En 2021, 64% des emballages Ã©taient recyclÃ©s en Europe. La Belgique se hisse donc en  premiÃ¨re place du podium en Ã©tant le pays qui recycle le plus les dÃ©chets emballÃ©s.</p> 
+<p> Concernant son <strong> objectif 2025 </strong>, lEurope est encore en dessous de son objectif den recycler au moins 65%. On remarque aussi une grande disparitÃ© entre certains pays europÃ©ens. Si la Belgique montre lexemple avec un taux de recyclage de 80,4%, certains pays comme Malte affichent un taux de 38,4%. 
+Malte et Roumanie font partie des Â« mauvais Ã©lÃ¨ves Â» (avec 38,4% et 39,9% en 2021 et 2020). Et derriÃ¨re la Belgique, viennent les Pays-Bas (76,6%), le Luxembourg (73,7%), lItalie (72,9%) et les autres pays suivent.</p> 
+<h2> L'Ã©volution du recyclage en Belgique </h2>
 
 
 
